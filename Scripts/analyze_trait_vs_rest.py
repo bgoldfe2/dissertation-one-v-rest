59c5f1c3d6417f2c8dff77d0437696e4eefdc17e
bruce-goldfeder-asetpartners <bruce.goldfeder@asetpartners.com>
2023-09-10 17:37:16 -0400

    completed TAWT and saved output for base models

--- Scripts/analyze_trait_vs_rest.py
+++ Scripts/analyze_trait_vs_rest.py
@@ -11,6 +11,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import os
 import pprint
+import json
 
 def get_results(run_folder):
     # Flag that identifies output results
@@ -29,7 +30,7 @@ def get_results(run_folder):
 
     return results_file_list
 
-def tawt(run_folder):
+def tawt(run_folder, Threshold=0.25):
     
     rsf = get_results(run_folder)
     print(rsf)
@@ -39,27 +40,27 @@ def tawt(run_folder):
     for results in rsf:    
         # file trait identifier during loop
         file_trt = results.split('---')[0]
-        print('The file trait this iteration is ',file_trt)
+        #print('The file trait this iteration is ',file_trt)
 
         # Set the file location for one v rest runs        
         file = ''.join([run_folder,'Output/',results])
 
         df = pd.read_csv(file)
         total_all = len(df)
-        print(total_all)
-        print(df.columns)
+        #print(total_all)
+        #print(df.columns)
         # Number in per label that are correct
         df['match'] = df['target']==df['y_pred']
         
         # Number in per label that are correct
         count = df.groupby('label').size()
-        print("count is of type ", type(count))
-        print("keys are ", count.axes)
-        print("get value for ", file_trt, " ", count.get(file_trt))
-        print("Size of each trait \n", count)
+        #print("count is of type ", type(count))
+        #print("keys are ", count.axes)
+        #print("get value for ", file_trt, " ", count.get(file_trt))
+        #print("Size of each trait \n", count)
 
         # Add each trait one-vs-rest to df_list
-        df_dict[file_trt] = df
+        df_dict[file_trt] = df.copy()
     
     df_Age = df_dict.get('Age')
     df_Ethnic = df_dict.get('Ethnicity')
@@ -68,17 +69,22 @@ def tawt(run_folder):
     df_Other = df_dict.get('Others')
     df_Religion = df_dict.get('Religion')
 
+    
+
     print(len(df_Age), " should be equal to ", len(df_Ethnic))
 
-    print("age", df_Age[['label','target','y_pred','match', 'prob-trt', 'prob-not-trt']].iloc[0], 
-          'Ethnicity', df_Ethnic[['label','target','y_pred','match', 'prob-trt', 'prob-not-trt']].iloc[0],
-          'Gender', df_Gender[['label','target','y_pred','match', 'prob-trt', 'prob-not-trt']].iloc[0],
-          'Notcb', df_Notcb[['label','target','y_pred','match', 'prob-trt', 'prob-not-trt']].iloc[0],
-          'Others', df_Other[['label','target','y_pred','match', 'prob-trt', 'prob-not-trt']].iloc[0],
-          'Religion', df_Religion[['label','target','y_pred','match', 'prob-trt', 'prob-not-trt']].iloc[0])
+    print("age\n", df_Age[['label','target','y_pred','match', 'prob-trt', 'prob-not-trt']].iloc[0], 
+          'Ethnicity\n', df_Ethnic[['label','target','y_pred','match', 'prob-trt', 'prob-not-trt']].iloc[0],
+          'Gender\n', df_Gender[['label','target','y_pred','match', 'prob-trt', 'prob-not-trt']].iloc[0],
+          'Notcb\n', df_Notcb[['label','target','y_pred','match', 'prob-trt', 'prob-not-trt']].iloc[0],
+          'Others\n', df_Other[['label','target','y_pred','match', 'prob-trt', 'prob-not-trt']].iloc[0],
+          'Religion\n', df_Religion[['label','target','y_pred','match', 'prob-trt', 'prob-not-trt']].iloc[0])
 
     cnt = 0
-    hits = []
+    correct = []
+    wrong = []
+    within_thresh = []
+    true_neg_thresh = []
     for i in range(len(df_Age)):
         out = []
         out.append(df_Age[['label','target','y_pred','match', 'prob-trt', 'prob-not-trt']].iloc[i].tolist())
@@ -89,7 +95,7 @@ def tawt(run_folder):
         out.append(df_Religion[['label','target','y_pred','match', 'prob-trt', 'prob-not-trt']].iloc[i].tolist())
         
         #pprint.pprint(out)
-
+        
         # gather data for other near misses within threshold
         # for ic in range(6):
         #     if float(out[ic][4]) > 0.1 and float(out[ic][4]) < 0.5:
@@ -103,19 +109,60 @@ def tawt(run_folder):
                 if out[ic][2]==0:
                     #print(traits.get(str(ic)), " is target and y_pred match")
                     cnt+=1
-                #else: # did not select target trait correctly
+                    correct_dict = dict()
+                    correct_dict['correct_lbl']=ic
+                    correct_dict['test_idx']=i
+                    correct_dict['trt_prob']=out[ic][4]
+                    correct.append(correct_dict)
+                elif out[ic][2]==1: # did not select target trait correctly
                     #print(traits.get(str(ic)), " is target and y_pred matched incorrectly")
-            #else: # not the target
-                #if out[ic][2]==0:
-                    #print(traits.get(str(ic)), " is not the target and y_pred matched incorrectly")
+                    if out[ic][4] > Threshold:
+                        thresh_dict = dict()
+                        thresh_dict['wrong_lbl']=ic
+                        thresh_dict['test_idx']=i
+                        thresh_dict['prob_trt']=out[ic][4]
+                        within_thresh.append(thresh_dict)
 
+            elif out[ic][1]==1: # not the target
+                if out[ic][2]==0:
+                    #print(traits.get(str(ic)), " is not the target and y_pred matched incorrectly")
+                    wrong_dict = dict()
+                    wrong_dict['wrong_lbl']=ic
+                    wrong_dict['test_idx']=i
+                    wrong_dict['prob_trt']=out[ic][4]
+                    wrong.append(wrong_dict)
+                elif out[ic][2]==1:  # True Negative but check for threshold
+                    if out[ic][4] > Threshold:
+                        tn_dict = dict()
+                        tn_dict['true_neg_lbl']=ic
+                        tn_dict['test_idx']=i
+                        tn_dict['prob_trt']= out[ic][4]
+                        true_neg_thresh.append(tn_dict)
+
+        #print('wrong list is \n', wrong)
+        #print('correct list is \n', correct)
+        
     print("accuracy is ", cnt/9541)
-    # Threshold values description
-    # avg = sum(hits)/len(hits)
-    # print("total between .1 and .5 is ", len(hits))
-    # print("average between .1 and .5 is ", avg)  
-    # df_result = pd.DataFrame(hits)
-    # print(df_result.describe())    
+    print("count and length of correct differnce = ",len(correct) - cnt)
+    wt_len = len(within_thresh)
+    print("length of within_thresh is ", wt_len)
+    print("wt of", Threshold, " accuracy is ", (cnt+wt_len)/9541)
+    print("length of true_neg_thresh is ", len(true_neg_thresh))
+    #print("True negative within threshold examples are\n", true_neg_thresh[0:4])
+    out_path = ''.join([run_folder, 'Ensemble/Output/'])
+    
+    #print(correct)
+    
+    with open(''.join([out_path, 'true_pos_outputfile']), 'w') as fout:
+        json.dump(correct, fout)
+    with open(''.join([out_path, 'false_neg_outputfile']), 'w') as fout:
+        json.dump(wrong, fout)
+    with open(''.join([out_path, 'false_pos_outputfile']), 'w') as fout:
+        json.dump(within_thresh, fout)
+    with open(''.join([out_path, 'true_neg_outputfile']), 'w') as fout:
+        json.dump(true_neg_thresh, fout)
+
+       
 
 
 
@@ -161,6 +208,7 @@ def parse_tvn(run_folder):
         #print("the type of the confusion matrix is ", type(cm))
         # Print the confusion matrix
         print(cm)
+        
         fig1, ax = plt.subplots()
         plt.title(''.join(["Single Trait ", file_trt, " vs All the Rest"]))
         sns.heatmap(cm, annot=True, fmt='d')
@@ -175,6 +223,8 @@ def parse_tvn(run_folder):
         # Check to see the numbers add to 9541 = size of the test set
         cm_sum =  cm.sum()
         print("sum of numbers in cm is ", cm_sum) 
+
+
         
         df['false_pos'] = np.where(df['target']==0, 1, 0) & np.where(df['y_pred']==1, 1, 0)
         df['false_neg'] = np.where(df['target']==1, 1, 0) & np.where(df['y_pred']==0, 1, 0)
@@ -239,7 +289,7 @@ def parse_tvn(run_folder):
 
 if __name__=="__main__":
     test_run = '../Runs/2023-08-24_17_46_26--roberta-base/'
-    #parse_tvn(test_run)
+    # parse_tvn(test_run)
     #graph_by_trt(df, cm)
     
     tawt(test_run)
